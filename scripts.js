c646c16e6915668c37bfcab6503e883b2046f506
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-11-10 23:27:56 -0300

    added key interaction and update display fn

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+
+
+//variables initialized
+
 let displayValue = '0';
 let firstOperand = null;
 let secondOperand = null;
@@ -5,3 +9,22 @@ let firstOperator = null;
 let secondOperator = null;
 let result = null;
 const buttons = document.querySelectorAll('button');
+
+//event listener allows to use keyboard keys and simulate clicks
+
+window.addEventListener('keydown', function(e){
+    const key = document.querySelector(`button[data-key='${e.keyCode}']`);
+    key.click();
+});
+
+// update the display
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.innerText = displayValue;
+    if(displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
+}
+  
+updateDisplay();
